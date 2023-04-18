344f8e5effc9fd1e08a465d7cbcd2aadb9fa7257
Serjkeey <sergeychoha2012@gmail.com>
2023-04-18 18:32:08 +0300

    its finally work

--- my-repository.py
+++ my-repository.py
@@ -1,7 +1,13 @@
 print('Hi')
-
 input('push Enter to continue')
-a = int(input('number 1:  '))
-b = str(input(f'+, -, *, /'))
-c = int(input('number 2:  '))
-print(f'answer {a} {b} {c} = {a} + {c}')
+a = float(input('number 1:  '))
+b = input('+ | - | * | / | ')
+c = float(input('number 2:  '))
+if b == '+':
+    print(a + c)
+if b == '-':
+    print(a - c)
+if b == '*':
+    print(a * c)
+if b == '/':
+    print(a / c)
